eb3df6c34bdbc70278ac934979e9f21ea1538558
Darren LaCour <lacourd@icloud.com>
2023-03-29 22:12:52 -0500

    Chapter 3 Exercises part 1

---
+++ src/exercises/Playground.java
@@ -0,0 +1,19 @@
+package exercises;
+import java.util.Scanner;
+public class Playground {
+    public static void main(String[] args) {
+
+            System.out.println("Are you a space cadet? yes or no");
+            Scanner in = new Scanner(System.in);
+            String response = in.next();
+
+            switch (response) {
+                case "yes":
+                    System.out.println("Greetings cadet.");
+                case "no":
+                    System.out.println("Greetings normie.");
+                default:
+                    System.out.println("Are you an alien?");
+            }
+    }
+}
