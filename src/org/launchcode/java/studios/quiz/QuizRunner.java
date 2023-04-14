4a6946b89a6e29436c7c4a2d075c8fd004259e00
Darren LaCour <lacourd@icloud.com>
2023-04-14 15:41:27 -0500

    Tested True False

--- src/org/launchcode/java/studios/quiz/QuizRunner.java
+++ src/org/launchcode/java/studios/quiz/QuizRunner.java
@@ -13,6 +13,10 @@ public class QuizRunner {
         answers1.add("Thursday");
         MultipleChoice question1 = new MultipleChoice(q1, answers1, 1);
         question1.askQuestion();
+        String q2 = "The sky is blue.";
+        ArrayList<String> answers2 = new ArrayList<>();
+        TrueFalse question2 = new TrueFalse(q2, answers2, 1);
+        question2.askQuestion();
 //        Quiz myQuiz = new Quiz();
 //        myQuiz.addQuestion(question1);
 //        myQuiz.runQuiz();
